721f51180c667ac5489986f341e7619d3b5ce9d1
jpatel6125 <23it081@charusat.edu.in>
2025-04-26 13:59:53 +0530

    Minor bug fixes

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,10 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names or class name objects into a single string
+ * Merges Tailwind CSS classes properly using twMerge
+ */
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-} 
+  return twMerge(clsx(inputs));
+}
